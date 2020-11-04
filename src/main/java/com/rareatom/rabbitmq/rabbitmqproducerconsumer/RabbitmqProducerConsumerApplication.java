30b1e4d6521bd1ef64483f24d626372ae081ff94
I.Tyger <60700259+ityger2020@users.noreply.github.com>
2020-11-04 09:11:22 +0100

    Initial commit for producer and consumer .

---
+++ src/main/java/com/rareatom/rabbitmq/rabbitmqproducerconsumer/RabbitmqProducerConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.rareatom.rabbitmq.rabbitmqproducerconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.mtn.madapi.commons.rabbitmq.queuing", "com.mtn.system"})
+public class RabbitmqProducerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqProducerConsumerApplication.class, args);
+	}
+
+}
