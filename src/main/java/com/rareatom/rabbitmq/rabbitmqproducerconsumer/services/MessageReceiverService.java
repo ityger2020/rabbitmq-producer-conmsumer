dacac83b8f3c9eba44da4301eb031c4bce877c53
I.Tyger <60700259+ityger2020@users.noreply.github.com>
2020-11-10 15:39:07 +0100

    Working on the tweaks on the software .

--- src/main/java/com/rareatom/rabbitmq/rabbitmqproducerconsumer/services/MessageReceiverService.java
+++ src/main/java/com/rareatom/rabbitmq/rabbitmqproducerconsumer/services/MessageReceiverService.java
@@ -2,17 +2,26 @@ package com.rareatom.rabbitmq.rabbitmqproducerconsumer.services;
 
 import com.mtn.madapi.commons.rabbitmq.queuing.consumer.BaseMessageReceiver;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @Slf4j
 public class MessageReceiverService implements BaseMessageReceiver {
+
+    @Autowired
+    SendMessageIntegrationService integrationService;
+
+    AtomicInteger processCount = new AtomicInteger(0);
+
     @Override
     public <OutboundUSSDResponse> Mono<Void> processMessage(OutboundUSSDResponse response, Map<String, Object> map) {
-        log.info("UssdMessageRequests {}" , response);
+        log.info("UssdMessageRequests from system layer{}" , response);
+        processCount.incrementAndGet();
         return null;
     }
 }
