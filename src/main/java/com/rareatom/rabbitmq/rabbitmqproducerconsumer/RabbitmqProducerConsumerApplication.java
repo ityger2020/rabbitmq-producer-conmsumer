1844964d4ed4f10373ea1400254bb9a537339a70
I.Tyger <60700259+ityger2020@users.noreply.github.com>
2020-11-10 08:32:24 +0100

    Added the required dependencies .

--- src/main/java/com/rareatom/rabbitmq/rabbitmqproducerconsumer/RabbitmqProducerConsumerApplication.java
+++ src/main/java/com/rareatom/rabbitmq/rabbitmqproducerconsumer/RabbitmqProducerConsumerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.mtn.madapi.commons.rabbitmq.queuing", "com.mtn.system" , "com.rareatom.rabbitmq.rabbitmqproducerconsumer"})
 public class RabbitmqProducerConsumerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqProducerConsumerApplication.class, args);
 	}
